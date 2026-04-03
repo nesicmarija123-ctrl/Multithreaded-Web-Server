c6bc7e27d417838395712c31ea4f8e932958b58e
nesicmarija <nesicmarija123@gmail.com>
2026-04-03 16:09:51 +0200

    Sending Requests via Channels

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,8 +1,12 @@
-use std::thread;
+use std::{sync::{Mutex, mpsc}, thread};
+use std::sync::Arc;
 
 pub struct ThreadPool{
     workers: Vec<Worker>, 
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool{
     /// Create a new ThreadPool
@@ -14,13 +18,18 @@ impl ThreadPool{
     /// The 'new' function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool{
         assert!(size > 0);  //panic if size is less than one 
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size{
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool{ workers}
+        ThreadPool{ workers, sender}
     }
 
     pub fn execute<F>(&self, f: F)
@@ -37,8 +46,10 @@ struct Worker{
 }
 
 impl Worker{
-        fn new(id: usize) -> Worker{
-        let thread = thread::spawn(|| {});
+        fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker{
+        let thread = thread::spawn(|| {
+            receiver;
+        });
 
         Worker {id, thread }
     }
