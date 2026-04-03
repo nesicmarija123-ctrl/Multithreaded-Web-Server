fe81cac64b940b52bd1cdff4fc175df684f308fc
nesicmarija <nesicmarija123@gmail.com>
2026-04-03 15:38:30 +0200

    Compiler Driven Development

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,24 @@
-use std::fs;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::io::prelude::*; //contents inside of prelude module
+use std::{
+    fs,
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+use server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming(){
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(||{
+            handle_connection(stream);
+        });
+        
     }
 }
 
@@ -18,11 +27,16 @@ fn handle_connection(mut stream: TcpStream){
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
     let (status_line, filename) = 
     if buffer.starts_with(get){
         ("HTTP/1.1 200 OK", "index.html")
     }
-    else{
+    else if buffer.starts_with(sleep){
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "index.html")
+    } else{
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
@@ -35,4 +49,5 @@ fn handle_connection(mut stream: TcpStream){
         
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+
 }
--- server/target/debug/server.d
+++ server/target/debug/server.d
@@ -1,1 +1,1 @@
-C:\Users\marija.nesic\projects\Multithreaded-Web-Server\server\target\debug\server.exe: C:\Users\marija.nesic\projects\Multithreaded-Web-Server\server\src\main.rs
+C:\Users\marija.nesic\projects\Multithreaded-Web-Server\server\target\debug\server.exe: C:\Users\marija.nesic\projects\Multithreaded-Web-Server\server\src\lib.rs C:\Users\marija.nesic\projects\Multithreaded-Web-Server\server\src\main.rs
