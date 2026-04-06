f35bee1017f5500da62ab33cd80f3f7010bd1cea
nesicmarija <nesicmarija123@gmail.com>
2026-04-06 11:29:44 +0200

    Implementing Drop Trait on ThreadPool

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -6,7 +6,9 @@ pub struct ThreadPool{
     sender: mpsc::Sender<Job>,
 }
 
-struct Job;
+///struct Job; //change this into a type alias
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
 
 impl ThreadPool{
     /// Create a new ThreadPool
@@ -36,21 +38,43 @@ impl ThreadPool{
     where 
         F: FnOnce() + Send + 'static,
         {
-            
+            let job = Box::new(f);
+            self.sender.send(job).unwrap();
         }
+}
+
+impl Drop for ThreadPool{
+    fn drop(&mut self){
+        for worker in &mut self.workers{
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take(){
+                thread.join().unwrap();
+            }
+            worker.thread.join().unwrap();
+        }
+    }
 }
 
 struct Worker{
     id: usize,
-    thread: thread::JoinHandle<()>
+    thread: Option<thread::JoinHandle<()>>
 }
 
 impl Worker{
         fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker{
-        let thread = thread::spawn(|| {
-            receiver;
+        let thread = thread::spawn(move || loop{
+            let job = receiver
+            .lock()
+            .unwrap()
+            .recv()
+            .unwrap();
+
+            println!("Worker {} got a job; executing.", id);
+
+            job();
         });
 
-        Worker {id, thread }
+        Worker {id, thread: Some(thread) }
     }
 }
